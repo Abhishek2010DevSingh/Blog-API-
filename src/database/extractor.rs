4d6706946b7a0dfeaed4afc88a14dfa2f99f4cbd
Abhishek2010DevSingh <Abhishekkumar2010dev@gmail.com>
2025-03-01 14:54:27 +0530

    app error

---
+++ src/database/extractor.rs
@@ -0,0 +1,23 @@
+use axum::{
+    extract::{FromRef, FromRequestParts},
+    http::{StatusCode, request::Parts},
+};
+use sqlx::PgPool;
+
+pub struct DatabaseConnection(sqlx::pool::PoolConnection<sqlx::Postgres>);
+
+impl<S> FromRequestParts<S> for DatabaseConnection
+where
+    PgPool: FromRef<S>,
+    S: Send + Sync,
+{
+    type Rejection = (StatusCode, String);
+
+    async fn from_request_parts(_parts: &mut Parts, state: &S) -> Result<Self, Self::Rejection> {
+        let pool = PgPool::from_ref(state);
+
+        let conn = pool.acquire().await.map_err(internal_error)?;
+
+        Ok(Self(conn))
+    }
+}
---
+++ src/error.rs
@@ -0,0 +1,43 @@
+use axum::{
+    http::StatusCode,
+    response::{IntoResponse, Response},
+    Json,
+};
+use serde_json::json;
+use thiserror::Error;
+use tracing::error;
+
+#[derive(Debug, Error)]
+pub enum AppError {
+    #[error("Database error: {0}")]
+    DatabaseError(#[from] sqlx::Error),
+    #[error("Not Found: {0}")]
+    NotFound(String),
+    #[error("Bad Request: {0}")]
+    BadRequest(String),
+    #[error("Internal Server Error")]
+    InternalServerError,
+}
+
+impl IntoResponse for AppError {
+    fn into_response(self) -> Response {
+        error!("{}", self);
+
+        let (status, error_message) = match &self {
+            AppError::DatabaseError(_) => (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                "A database error occurred.",
+            ),
+            AppError::NotFound(message) => (StatusCode::NOT_FOUND, message.as_str()),
+            AppError::BadRequest(message) => (StatusCode::BAD_REQUEST, message.as_str()),
+            AppError::InternalServerError => (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                "An internal server error occurred.",
+            ),
+        };
+
+        let body = Json(json!({ "error": error_message }));
+
+        (status, body).into_response()
+    }
+}
