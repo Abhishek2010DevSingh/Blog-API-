fffc44965ef4b6d8dc8653b6f518a89793dc7928
Abhishek2010DevSingh <Abhishekkumar2010dev@gmail.com>
2025-03-01 15:23:59 +0530

    bug fix

--- src/database/connection.rs
+++ src/database/connection.rs
@@ -1,9 +1,10 @@
 use anyhow::Context;
 
 pub async fn db_connect() -> anyhow::Result<sqlx::Pool<sqlx::Postgres>> {
+    let database_url = std::env::var("DATABASE_URL").context("DATABASE_URL is not set")?;
     let pool = sqlx::postgres::PgPoolOptions::new()
         .max_connections(5)
-        .connect("postgres://postgres:password@localhost/axum")
+        .connect(&database_url)
         .await
         .context("Failed to connect to Postgres")?;
 
--- src/server/routes.rs
+++ src/server/routes.rs
@@ -1,17 +1,9 @@
-use axum::{Router, routing::get};
+use axum::Router;
 
-use crate::{database::extractor::DatabaseConnection, error::AppError, state::AppState};
+use crate::state::AppState;
 
 pub fn setup_routes(state: AppState) -> Router {
     Router::new()
-        .route("/", get(hello))
         .layer(tower_http::trace::TraceLayer::new_for_http())
         .with_state(state)
-}
-
-async fn hello(DatabaseConnection(mut conn): DatabaseConnection) -> Result<String, AppError> {
-    let result: Option<i32> = sqlx::query_scalar!("SELECT 1 + 1")
-        .fetch_one(&mut *conn)
-        .await?;
-    return Ok(format!("Hello, world! {}", result.unwrap()));
 }
