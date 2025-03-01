06961ab4e0aa335feedcdd1e6091309bfb89aaa5
Abhishek2010DevSingh <Abhishekkumar2010dev@gmail.com>
2025-03-01 14:55:54 +0530

    database connection extractor

--- src/database/extractor.rs
+++ src/database/extractor.rs
@@ -1,23 +1,23 @@
 use axum::{
     extract::{FromRef, FromRequestParts},
-    http::{StatusCode, request::Parts},
+    http::request::Parts,
 };
 use sqlx::PgPool;
 
-pub struct DatabaseConnection(sqlx::pool::PoolConnection<sqlx::Postgres>);
+use crate::error::AppError;
+
+pub struct DatabaseConnection(pub sqlx::pool::PoolConnection<sqlx::Postgres>);
 
 impl<S> FromRequestParts<S> for DatabaseConnection
 where
     PgPool: FromRef<S>,
     S: Send + Sync,
 {
-    type Rejection = (StatusCode, String);
+    type Rejection = AppError;
 
     async fn from_request_parts(_parts: &mut Parts, state: &S) -> Result<Self, Self::Rejection> {
         let pool = PgPool::from_ref(state);
-
-        let conn = pool.acquire().await.map_err(internal_error)?;
-
+        let conn = pool.acquire().await?;
         Ok(Self(conn))
     }
 }
