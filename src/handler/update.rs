4edce61c1c5dc35b36d594a0dfa93a0f63b3dfb2
Abhishek2010DevSingh <Abhishekkumar2010dev@gmail.com>
2025-03-02 10:18:43 +0530

    update handler

--- src/handler/update.rs
+++ src/handler/update.rs
@@ -1,16 +1,48 @@
 use axum::{extract::Path, Json};
 use axum_valid::Valid;
+use sqlx::query_as;
 
 use crate::{database::extractor::DatabaseConnection, error::AppError, model::blog::BlogPost};
 
 use super::create::BlogPostBody;
-
+/// Updates a blog post by its ID in the database.
+///
+/// # Arguments
+///
+/// * `DatabaseConnection(mut conn)`: A wrapper for the database connection.
+/// * `Path(id)`: The ID of the blog post to update.
+/// * `Valid(Json(payload))`: The validated JSON payload containing the updated blog post data.
+///
+/// # Returns
+///
+/// Returns a `Result` containing:
+/// - `Json(BlogPost)` if the update is successful.
+/// - `AppError::NotFound` if no blog post with the given ID exists.
+/// - `AppError` if an error occurs during the database query.
+///
+/// # Errors
+///
+/// This function will return an `AppError` if:
+/// - The database query fails.
+/// - The specified blog post does not exist.
+///
+/// # Example
+///
+/// ```
+/// PUT /posts/1
+/// {
+///   "title": "Updated Title",
+///   "content": "Updated content",
+///   "category": "Updated category",
+///   "tags": ["updated", "tags"]
+/// }
+/// ```
 pub async fn update_by_id(
     DatabaseConnection(mut conn): DatabaseConnection,
     Path(id): Path<i32>,
-    Valid(Json(value)): Valid<Json<BlogPostBody>>,
+    Valid(Json(payload)): Valid<Json<BlogPostBody>>,
 ) -> Result<Json<BlogPost>, AppError> {
-    let value = sqlx::query_as!(
+    let updated_post = query_as!(
         BlogPost,
         r#"
         UPDATE blog_posts 
@@ -21,12 +53,16 @@ pub async fn update_by_id(
         WHERE id = $5
         RETURNING *;
         "#,
-        value.title,
-        value.content,
-        value.category,
-        &value.tags,
+        payload.title,
+        payload.content,
+        payload.category,
+        &payload.tags,
         id
     )
-    .fetch_optional(&mut *conn)
-    .await?;
+    .fetch_one(&mut *conn)
+    .await
+    .map(Json)
+    .map_err(|_| AppError::NotFound("Blog post not found".into()))?;
+
+    Ok(updated_post)
 }
